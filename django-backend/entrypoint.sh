0bbc7cd21317de39424fd923f487e3065daae7b7
scrickman <scrickman@isystematics.com>
2022-01-18 15:48:41 -0500

    Change to settings.py to retrieve env vars in K8S.

---
+++ django-backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+cd /app
+
+python3 manage.py collectstatic --noinput
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+#python3 manage.py runserver 0.0.0.0:8000
+
+uwsgi --uid 33 --gid 33 --socket :8000 --master --enable-threads --module mission.wsgi
