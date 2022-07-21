629cfc6c24a4969661ae239f4620b7cd2551144f
Dmitriy <dimkahristichenko@gmail.com>
2022-07-21 13:08:00 +0200

    Added Sync Module Action for Minion

--- django-backend/saltmaster/urls.py
+++ django-backend/saltmaster/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     ManageKeyView,
+    SyncModuleView,
     KeyValue
 )
 
 urlpatterns = [
     path('manage-key/<int:minion_id>/<str:operation>/', ManageKeyView.as_view(), name='manage-key'),
     path('redis-key-value/<path:key>/', KeyValue.as_view(), name='redis-key-value'),
+    path('sync-module/<int:minion_id>/', SyncModuleView.as_view(), name='sync-module'),
 ]
